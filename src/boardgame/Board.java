eca69aabfc534704579111a8ca4183201133c1e6
Victor Holanda <jvictorweb21@gmail.com>
2022-03-20 12:45:11 -0300

    Moving pieces

--- src/boardgame/Board.java
+++ src/boardgame/Board.java
@@ -5,7 +5,7 @@ public class Board {
 	private int rows;
 	private int columns;
 	private Piece[][] pieces;
-	
+
 	public Board(int rows, int columns) {
 		if (rows < 1 || columns < 1) {
 			throw new BoardException("Error creating board: there must be at least 1 row and 1 column");
@@ -29,30 +29,43 @@ public class Board {
 		}
 		return this.pieces[row][column];
 	}
-	
+
 	public Piece piece(Position pos) {
 		if (!positionExists(pos)) {
 			throw new BoardException("Position not on the board");
 		}
 		return this.pieces[pos.getRow()][pos.getColumn()];
 	}
-	
+
 	public void placePiece(Piece piece, Position pos) {
-		if ( thereIsaPiece(pos)) {
+		if (thereIsaPiece(pos)) {
 			throw new BoardException("There is already a piece on position " + pos);
 		}
 		this.pieces[pos.getRow()][pos.getColumn()] = piece;
 		piece.position = pos;
 	}
-	
+
+	public Piece removePiece(Position pos) {
+		if (!positionExists(pos)) {
+			throw new BoardException("Position not on the board");
+		}
+		if(piece(pos) == null) {
+			return null;
+		}
+		Piece aux = piece(pos);
+		aux.position = null;
+		this.pieces[pos.getRow()][pos.getColumn()] = null;
+		return aux;
+	}
+
 	private boolean positionExists(int row, int column) {
 		return row >= 0 && row < this.rows && column >= 0 && column < this.columns;
 	}
-	
+
 	public boolean positionExists(Position pos) {
 		return positionExists(pos.getRow(), pos.getColumn());
 	}
-	
+
 	public boolean thereIsaPiece(Position pos) {
 		if (!positionExists(pos)) {
 			throw new BoardException("Position not on the board");
