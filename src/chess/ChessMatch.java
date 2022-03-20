0df6e7ed6a09a55932f0d31b2274053880a7f813
Victor Holanda <jvictorweb21@gmail.com>
2022-03-20 12:12:23 -0300

    Little improvement in board printing

--- src/chess/ChessMatch.java
+++ src/chess/ChessMatch.java
@@ -7,34 +7,42 @@ import chess.pieces.Rook;
 public class ChessMatch {
 
 	private Board board;
-	
+
 	public ChessMatch() {
-		this.board = new Board(8,8);
+		this.board = new Board(8, 8);
 		initialSetup();
 	}
-	
-	public ChessPiece[][] getPieces(){
+
+	public ChessPiece[][] getPieces() {
 		ChessPiece[][] mat = new ChessPiece[this.board.getRows()][this.board.getColumns()];
-		for (int i=0; i< this.board.getRows(); i++) {
-			for (int j=0; j < this.board.getColumns(); j++) {
-				mat[i][j] = (ChessPiece) this.board.piece(i,j);
+		for (int i = 0; i < this.board.getRows(); i++) {
+			for (int j = 0; j < this.board.getColumns(); j++) {
+				mat[i][j] = (ChessPiece) this.board.piece(i, j);
 			}
 		}
 		return mat;
 	}
-	
+
 	private void placeNewPiece(char column, int row, ChessPiece piece) {
-		this.board.placePiece(
-					piece,
-					new ChessPosition(column, row).toPosition()
-				);
+		this.board.placePiece(piece, new ChessPosition(column, row).toPosition());
 	}
-	
+
 	private void initialSetup() {
-		placeNewPiece('b', 6, new Rook(this.board,Color.WHITE));
-		placeNewPiece('e', 8, new King(this.board,Color.BLACK));
-		placeNewPiece('e', 1, new King(this.board,Color.WHITE));
-		
+
+		placeNewPiece('c', 1, new Rook(board, Color.WHITE));
+		placeNewPiece('c', 2, new Rook(board, Color.WHITE));
+		placeNewPiece('d', 2, new Rook(board, Color.WHITE));
+		placeNewPiece('e', 2, new Rook(board, Color.WHITE));
+		placeNewPiece('e', 1, new Rook(board, Color.WHITE));
+		placeNewPiece('d', 1, new King(board, Color.WHITE));
+
+		placeNewPiece('c', 7, new Rook(board, Color.BLACK));
+		placeNewPiece('c', 8, new Rook(board, Color.BLACK));
+		placeNewPiece('d', 7, new Rook(board, Color.BLACK));
+		placeNewPiece('e', 7, new Rook(board, Color.BLACK));
+		placeNewPiece('e', 8, new Rook(board, Color.BLACK));
+		placeNewPiece('d', 8, new King(board, Color.BLACK));
+
 	}
-	
+
 }
